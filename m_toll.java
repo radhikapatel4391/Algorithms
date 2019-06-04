c9e245c620b861be367b94b986e940dd24cd8fec
radhikapatel4391 <radhika.patel4391@gmail.com>
2019-06-04 09:43:54 -0400

    update

--- m_toll.java
+++ m_toll.java
@@ -1,165 +1,95 @@
 import java.util.*;
 
 
-public class m_toll {
+public class m_maxflow {
 	class Edge
 	{
 		String startNode;
 		String endNode;
-		
-		Edge(String start,String end)
+		int weight;
+		Edge(String start,String end,int weight)
 		{
 			this.startNode = start;
 			this.endNode = end;
-			
+			this.weight = weight;
 		}
 	}
+
 	public static void main(String[] args) {
-		m_toll object = new m_toll();
 		Scanner sc = new Scanner(System.in);
-		int caseCount = 0;
+		m_maxflow object = new m_maxflow();
 		while(sc.hasNext())
 		{
-			caseCount++;			
-			int edgecount = sc.nextInt();
-			if (edgecount==-1)
-					return;
-			System.out.printf("Case %d: ",caseCount);
-<<<<<<< HEAD
-			ArrayList<Edge> edges = new ArrayList<Edge>(2*edgecount);
-=======
-			ArrayList<Edge> edges = new ArrayList<Edge>(edgecount);
->>>>>>> parent of 97c8f38... update
+			String start = sc.next("[A-Z]");
+			String end = sc.next("[A-Z]");
+			ArrayList<Edge> edges = new ArrayList<Edge>();
 			Set<String> vertices = new HashSet<String>();
-			while(edgecount>0)
+			String s = sc.next();
+			while(!s.contains("*"))
 			{
-				edgecount--;
-				String s = sc.next("[A-Za-z]");				
-				String e = sc.next("[A-Za-z]");
+				if(!s.matches("[A-Z]"))
+					s = sc.next("[A-Z]");
+				String e = sc.next("[A-Z]");
 				vertices.add(e);				
-				vertices.add(s);			
-<<<<<<< HEAD
-				edges.add(object.new Edge(s,e));
-				edges.add(object.new Edge(e,s));	
-=======
-				edges.add(object.new Edge(s,e));				
->>>>>>> parent of 97c8f38... update
+				vertices.add(s);
+				int w = Integer.parseInt(sc.next("\\d+"));
+				edges.add(object.new Edge(s,e,w));
+				s = sc.next();
 			}
-			int n = sc.nextInt();
-			String start = sc.next("[A-Za-z]");
-			String end = sc.next("[A-Za-z]");
-			
-			HashMap<String,ArrayList<Edge>> graph = new HashMap<String,ArrayList<Edge>>();
-			HashMap<String,Integer> mtable = new HashMap<String,Integer>(vertices.size());
-			for(String s:vertices)
+			if(vertices.size()<=0)
 			{
-				mtable.put(s, 0);
+				System.out.println("NIL");
+				
+			}else{
+				HashMap<String,ArrayList<Edge>> graph = new HashMap<String,ArrayList<Edge>>();
+				HashMap<String,Integer> mtable = new HashMap<String,Integer>(vertices.size());
+				for(String ss:vertices)
+				{
+					mtable.put(ss, Integer.MAX_VALUE);
+				}
+				for(Edge edge:edges)
+				{
+					String key = edge.startNode;
+					ArrayList<Edge> value = graph.get(key);
+					if(value==null)
+						value = new ArrayList<Edge>();
+					value.add(edge);
+					graph.put(key,value);
+				}
+				int ans = findMaxofMin(graph,mtable,start,end);
+				if(ans==Integer.MAX_VALUE || ans==Integer.MIN_VALUE)
+					System.out.println("NIL");
+				else
+					System.out.println(ans);
 			}
 			
-			boolean hasUppercase = !end.equals(end.toLowerCase());
-			if(hasUppercase)
-			{
-				double a = 20/19d;
-				 a = n*a; 
-				n= (int)Math.ceil(a);
-			}
-			else
-				n = n+1;
-			mtable.put(end, n);
+		}
+		 sc.close();
+	}
+	
+	public static int findMaxofMin(HashMap<String,ArrayList<Edge>> graph,HashMap<String,Integer> mtable,String start,String end)
+	{
+		int max = Integer.MIN_VALUE;
+		
+		if(start.charAt(0)==end.charAt(0))
+			return Integer.MAX_VALUE;
+		int ansSub = mtable.get(start);
+		if(ansSub!=Integer.MAX_VALUE && ansSub!=-1)
+			return ansSub;
+		ArrayList<Edge>  edges = graph.get(start);
+		mtable.put(start,-1);
+		if(edges!=null)
+		{
+					
 			for(Edge edge:edges)
 			{
-				String key = edge.startNode;
-<<<<<<< HEAD
-				ArrayList<Edge> value = graph.get(key);
-				if(value==null)
-					value = new ArrayList<Edge>();
-				value.add(edge);
-				graph.put(key,value);
-=======
-				graph.computeIfAbsent(key,k->new ArrayList<Edge>()).add(edge);
->>>>>>> parent of 97c8f38... update
+				if(mtable.get(edge.endNode)!=-1)
+					max = Math.max(max,Math.min(edge.weight,findMaxofMin(graph,mtable,edge.endNode,end)));
 			}
-			int ans = findMinVlaue(graph,mtable,start,end);
-			 hasUppercase = !start.equals(start.toLowerCase());
-			if(hasUppercase)
-			{
-				double d = 20D;
-				ans = (int) (ans - Math.ceil((ans/d)));
-			}
-			else
-			{
-				ans = ans-1;
-			}			
-			System.out.println(ans);
-		}
-		 sc.close();			
-		}
-	public static int findMinVlaue(HashMap<String,ArrayList<Edge>> graph,HashMap<String,Integer> mtable,String start,String end)
-	{
+		}	
+		mtable.put(start,max);
 		
-		int ans = mtable.get(start);
-		if(start==end)
-			return ans;
-<<<<<<< HEAD
-//		if(ans==-1) //lopp case...
-//			return Integer.MAX_VALUE;
-		if(ans!=0 && ans!=Integer.MAX_VALUE)
-			return ans;
-		mtable.put(start,-1);
-		int min = Integer.MAX_VALUE;
-		ArrayList<Edge> list = graph.get(start);
-		if(list!=null)
-		{
-		    for(Edge e:list)
-		{
-		    if(mtable.get(e.endNode)!=-1)
-		    {
-		    	int x = findMinVlaue(graph,mtable,e.endNode,end);			
-				min = Math.min(min, x);
-		    }
-		}
-		}
-		if(min<Integer.MAX_VALUE)
-		{
-			boolean hasUppercase = !start.equals(start.toLowerCase());
-			//	boolean hasLowercase = !start.equals(start.toUpperCase());
-				if(hasUppercase)
-				{
-					double a = 20/19d;
-					 a = min*a; 
-					min= (int)Math.ceil(a);
-				}
-				else
-					min = min+1;
-		}
-=======
-		if(ans==-1) //lopp case...
-			return 10000;
-		if(ans!=0)
-			return ans;
-		mtable.put(start,-1);
-		int min = Integer.MAX_VALUE;
-		for(Edge e:graph.get(start))
-		{
-			int x = findMinVlaue(graph,mtable,e.endNode,end);			
-			min = Math.min(min, x);
-		}
-		boolean hasUppercase = !start.equals(start.toLowerCase());
-		//	boolean hasLowercase = !start.equals(start.toUpperCase());
-			if(hasUppercase)
-			{
-				double a = 20/19d;
-				 a = min*a; 
-				min= (int)Math.ceil(a);
-			}
-			else
-				min = min+1;
->>>>>>> parent of 97c8f38... update
-		mtable.put(start,min);
-		return min;
-		
+		return max;
 	}
 
-	}
-
-
+}
